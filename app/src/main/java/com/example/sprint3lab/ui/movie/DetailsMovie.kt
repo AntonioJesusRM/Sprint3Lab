123cd4df82e917e0bbdd02312f300dab785161e3
AntonioJesusRM <antjrm95@gmail.com>
2024-05-12 13:22:04 +0200

    add activity datails xml

---
+++ app/src/main/java/com/example/sprint3lab/ui/movie/DetailsMovie.kt
@@ -0,0 +1,21 @@
+package com.example.sprint3lab.ui.movie
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.sprint3lab.R
+
+class DetailsMovie : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_details_movie)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
